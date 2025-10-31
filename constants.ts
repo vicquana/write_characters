6471c62c6675e8686219560967a8d54af4af4d5d
vicquana_MBA <cjh425@gmail.com>
2025-10-31 12:40:32 -0500

    added char

--- constants.ts
+++ constants.ts
@@ -2,17 +2,17 @@ export const PRACTICE_CHARACTERS: string[] = [
   "佛", // one
   "神", // two
   "爸", // three
-  "土", // person
+  "公", // person
   "金", // big
   "河", // sky, day
   "海", // mouth
   "刀", // sun, day
   "全", // moon, month
   "在", // water
-  "火", // fire
-  "山", // mountain
-  "中", // middle
-  "水", // country
-  "上", // up
-  "下", // down
+  "草", // fire
+  "森", // mountain
+  "林", // middle
+  "樹", // country
+  "數", // up
+  "學", // down
 ];
