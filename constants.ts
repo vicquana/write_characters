f017f7c99890b21418a9c364556a42541cb07666
vicquana_MBA <cjh425@gmail.com>
2025-10-31 19:24:58 -0500

    加字

--- constants.ts
+++ constants.ts
@@ -1,3 +1,5 @@
-export const PRACTICE_CHARACTER_STRING = "佛神爸公金河海刀全在草森林樹數學";
+export const PRACTICE_CHARACTER_STRING = "佛神平公金河海刀劍全在草森林樹數學";
 
-export const PRACTICE_CHARACTERS: string[] = Array.from(PRACTICE_CHARACTER_STRING);
+export const PRACTICE_CHARACTERS: string[] = Array.from(
+  PRACTICE_CHARACTER_STRING
+);
